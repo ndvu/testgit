6c3a44eea9cd55ec7d3e669dc424e5c0da82ead8
ndvu <ndungvu93@gmail.com>
2019-08-02 15:37:53 +0700

    Update mastewr

--- TestMaven/src/main/java/test/com/App.java
+++ TestMaven/src/main/java/test/com/App.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.print("master");
+		System.out.print("master1");
 		try {
 			SpringApplication.run(App.class, args);
 		} catch (Exception e) {
