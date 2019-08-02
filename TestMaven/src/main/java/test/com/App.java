8f4734e6f4b5712807c1931800a90537b960b599
ndvu <ndungvu93@gmail.com>
2019-08-02 10:40:47 +0700

    sdf

---
+++ TestMaven/src/main/java/test/com/App.java
@@ -0,0 +1,20 @@
+package test.com;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
+//
+//@SpringBootApplication
+//@ComponentScan(basePackages = "test.com")
+public class App {
+
+	public static void main(String[] args) {
+		System.out.print("asd");
+		try {
+//			SpringApplication.run(App.class, args);
+		} catch (Exception e) {
+			System.out.print(e);
+		}
+	}
+
+}
---
+++ TestMaven/src/main/java/test/com/TestConfiguration.java
@@ -0,0 +1,10 @@
+//package test.com;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//
+//@Configuration
+//@PropertySource("classpath:test.properties")
+//public class TestConfiguration {
+//
+//}
