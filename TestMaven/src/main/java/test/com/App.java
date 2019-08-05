452cdd278a8ab5272e6acdf486c26442278ee690
ndvu <ndungvu93@gmail.com>
2019-08-05 14:59:40 +0700

    20

--- TestMaven/src/main/java/test/com/App.java
+++ TestMaven/src/main/java/test/com/App.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.print("master19");
+		System.out.print("master20");
 		System.out.print("test2");
 		System.out.print("test3");
 		try {
