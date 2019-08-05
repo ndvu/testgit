f99d96c486685b63fbae60a6b46bbfe3756ba64a
ndvu <ndungvu93@gmail.com>
2019-08-05 16:17:18 +0700

    33

--- TestMaven/src/main/java/test/com/App.java
+++ TestMaven/src/main/java/test/com/App.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.print("master32");
+		System.out.print("master33");
 		System.out.print("test2");
 		System.out.print("test3");
 		try {
